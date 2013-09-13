0185a4e5b2177dfe68740bc0f2c21139ccfffcb6
Robert Gibboni <robertg@berkeley.edu>
2013-09-12 18:00:12 -0700

    Added command line access with argument parsing.

--- CalCalc.py
+++ CalCalc.py
@@ -1,6 +1,13 @@
 #!/usr/bin python
+import argparse
 
 def calculate(query="2*3"):
 
 	result = eval(query)
 	print result
+
+if __name__ == '__main__':
+	argparser = argparse.ArgumentParser(description = 'Simple statement evaluator')
+	argparser.add_argument('query', help = 'The query you wish to evaluate')
+	args = argparser.parse_args()
+	calculate(args.query)
