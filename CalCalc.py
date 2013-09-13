5e2a305c4dbb9587f99415464d02295df5061b59
Robert Gibboni <robertg@berkeley.edu>
2013-09-12 18:03:29 -0700

    Started adding WolframAlpha support.

--- CalCalc.py
+++ CalCalc.py
@@ -4,10 +4,26 @@ import argparse
 def calculate(query="2*3"):
 
 	result = eval(query)
+	print 'Result: '
 	print result
 
+
+def calculate_wolframalpha(query="What is my IP address?"):
+
+	print 'WolframAlpha result:'
+	print 'temp'
+	
+
 if __name__ == '__main__':
-	argparser = argparse.ArgumentParser(description = 'Simple statement evaluator')
-	argparser.add_argument('query', help = 'The query you wish to evaluate')
+
+	argparser = argparse.ArgumentParser(description='Simple statement evaluator')
+	
+	argparser.add_argument('query', help='The query you wish to evaluate')
+	argparser.add_argument('-w', action='store_true', dest='use_wolframalpha', default=False)
+
 	args = argparser.parse_args()
-	calculate(args.query)
+
+	if use_wolframalpha:
+		calculate_wolframalpha(args.query)
+	else:
+		calculate(args.query)
