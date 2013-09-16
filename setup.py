3fa1aebc3ef2afcaeba5205d3d962716239bc9c3
Robert Gibboni <robertg@berkeley.edu>
2013-09-16 15:46:17 -0700

    Added READMEs and setup.py files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='CalCalc',
+	version='1.0',
+	license='License_to_not_kill',
+	author='Robert Gibboni',
+	author_email='galileo@gmail.com',
+	py_modules=['CalCalc'])
