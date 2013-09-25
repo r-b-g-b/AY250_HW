edfcc5845bebb163a55222db67a245ccfa9e2a5c
Robert Gibboni <robertg@berkeley.edu>
2013-09-25 15:54:09 -0700

    Formatting y and x axes, added ticks, labels, and legend

--- hw3/hw3_2.py
+++ hw3/hw3_2.py
@@ -2,22 +2,46 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import pandas as pd
 
+# load data
+yahoo = pd.read_csv('hw3_2_data/yahoo_data.txt', sep='\t')
 goog = pd.read_csv('hw3_2_data/google_data.txt', sep='\t')
 temps = pd.read_csv('hw3_2_data/ny_temps.txt', sep='\t')
-yahoo = pd.read_csv('hw3_2_data/yahoo_data.txt', sep='\t')
 
-fig, ax_stocks = plt.subplots(figsize=(6, 12))
+# initialize figure
+fig, ax_stocks = plt.subplots(figsize=(10, 6))
 ax_temp = ax_stocks.twinx()
 
-ax_stocks.plot(goog['Modified Julian Date'].values, goog['Stock Value'].values, c='b')
-ax_stocks.plot(yahoo['Modified Julian Date'].values, yahoo['Stock Value'].values, c='purple')
-ax_temp.plot(temps['Modified Julian Date'].values, temps['Max Temperature'].values, 'r--')
+# plot to axes
+l1 = ax_stocks.plot(yahoo['Modified Julian Date'].values, yahoo['Stock Value'].values, c='purple', label='Yahoo! Stock Value')
+l2 = ax_stocks.plot(goog['Modified Julian Date'].values, goog['Stock Value'].values, c='b', label='Google Stock Value')
+l3 = ax_temp.plot(temps['Modified Julian Date'].values, temps['Max Temperature'].values, 'r--', label='NY Mon. High Temp')
 
+ax_stocks.set_ylabel('Value (Dollars)')
+ax_temp.set_ylabel(u'Temperature (\xb0F)')
+ax_stocks.set_xlabel('Date (MJD)')
+
+# set title
+
+# format y-axis
+ax_stocks.yaxis.set_major_locator(MultipleLocator(100))
+ax_stocks.yaxis.set_minor_locator(MultipleLocator(20))
+
+# format x-axis
+ax_stocks.set_xlim([48800, 55600])
+ax_stocks.xaxis.set_major_locator(MultipleLocator(1000))
+ax_stocks.xaxis.set_minor_locator(MultipleLocator(200))
+
+# format y-axis
 ax_temp.set_ylim([-150, 100])
 ax_temp.yaxis.set_major_locator(MultipleLocator(50))
 ax_temp.yaxis.set_minor_locator(MultipleLocator(10))
 
+# add legend
+lines = l1+l2+l3
+labels = [l.get_label() for l in lines]
+ax_temp.legend(lines, labels, loc='center left', frameon=False, fontsize=10)
+
 plt.show()
 plt.draw()
 
-
+fig.savefig('stocks_RG.png')
