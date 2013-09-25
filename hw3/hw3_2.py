aad3b237ba30e46ba04af99f468559e921353ae0
Robert Gibboni <robertg@berkeley.edu>
2013-09-25 16:06:25 -0700

    Save figure when complete

--- hw3/hw3_2.py
+++ hw3/hw3_2.py
@@ -8,19 +8,20 @@ goog = pd.read_csv('hw3_2_data/google_data.txt', sep='\t')
 temps = pd.read_csv('hw3_2_data/ny_temps.txt', sep='\t')
 
 # initialize figure
-fig, ax_stocks = plt.subplots(figsize=(10, 6))
+fig, ax_stocks = plt.subplots(figsize=(9, 6))
 ax_temp = ax_stocks.twinx()
 
 # plot to axes
-l1 = ax_stocks.plot(yahoo['Modified Julian Date'].values, yahoo['Stock Value'].values, c='purple', label='Yahoo! Stock Value')
-l2 = ax_stocks.plot(goog['Modified Julian Date'].values, goog['Stock Value'].values, c='b', label='Google Stock Value')
-l3 = ax_temp.plot(temps['Modified Julian Date'].values, temps['Max Temperature'].values, 'r--', label='NY Mon. High Temp')
+l1 = ax_stocks.plot(yahoo['Modified Julian Date'].values, yahoo['Stock Value'].values, c='purple', lw=1.5, label='Yahoo! Stock Value')
+l2 = ax_stocks.plot(goog['Modified Julian Date'].values, goog['Stock Value'].values, c='b', lw=1.5, label='Google Stock Value')
+l3 = ax_temp.plot(temps['Modified Julian Date'].values, temps['Max Temperature'].values, 'r', lw=1.5, dashes=[5,2], label='NY Mon. High Temp')
 
 ax_stocks.set_ylabel('Value (Dollars)')
 ax_temp.set_ylabel(u'Temperature (\xb0F)')
 ax_stocks.set_xlabel('Date (MJD)')
 
 # set title
+ax_stocks.set_title('New York Temperature, Google, and Yahoo!')
 
 # format y-axis
 ax_stocks.yaxis.set_major_locator(MultipleLocator(100))
@@ -39,9 +40,9 @@ ax_temp.yaxis.set_minor_locator(MultipleLocator(10))
 # add legend
 lines = l1+l2+l3
 labels = [l.get_label() for l in lines]
-ax_temp.legend(lines, labels, loc='center left', frameon=False, fontsize=10)
+ax_temp.legend(lines, labels, loc='center left', frameon=False, fontsize=12)
 
-plt.show()
 plt.draw()
+plt.show()
 
 fig.savefig('stocks_RG.png')
