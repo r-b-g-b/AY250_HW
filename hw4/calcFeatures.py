ad836b04fa39256fc7755dfa5153bd8781fab8de
Robert Gibboni <robertg@berkeley.edu>
2013-09-30 16:01:00 -0700

    Added HOG feature calculation

--- hw4/calcFeatures.py
+++ hw4/calcFeatures.py
@@ -12,7 +12,9 @@ from sklearn.decomposition import PCA
 numproc = cpu_count()
 
 def calc_hog(fpaths):
+
     hogs = []
+    
     for fpath in fpaths:
         category, _ = os.path.split(fpath)
         img = imread(fpath)
@@ -23,6 +25,8 @@ def calc_hog(fpaths):
         img_hog = hog(img_resize)
         hogs.append(img_hog)
 
+    return hogs
+
 def map_hog(fpaths):
 
     fpaths_split = split_seq(fpaths, numproc)
@@ -30,8 +34,24 @@ def map_hog(fpaths):
     # initialize pool?
     p = Pool(numproc)
     # map jobs to processors
-    result = p.map_async(func, fpaths_split)
+    result = p.map_async(calc_hog, fpaths_split)
     poolresult = result.get()
+
+    x = np.array(poolresult[0])
+    for i in range(1, numproc):
+        x = np.vstack((x, poolresult[i]))
+
+    n_components = 20
+    pca = PCA(n_components=n_components)
+    x2 = pca.fit_transform(x)
+    fnames = [os.path.split(i)[1] for i in fpaths]
+    categories = [i.split('_')[0] for i in fnames]
+
+    df = pd.DataFrame(x2, columns=['feat_hog_%2.2u'%i for i in range(1, n_components+1)])
+    df['fpath'] = fnames
+    df['category'] = categories
+
+    df.to_csv('hog.csv')
 
 
 def calc_spatial_power_ratio(fpaths):
@@ -42,15 +62,12 @@ def calc_spatial_power_ratio(fpaths):
         img = imread(fpath)
         if len(img.shape)==3:
             img = rgb2gray(img)
+        img_resize = resize(img, (300, 400))
+        IMG = np.fft.rfft2(img_resize)
+
 
     d = dict(category=categories, fpath=fpaths, feat_hi_lo_ratio=power_ratio)
     return pd.DataFrame(d)
-
-
-def calc_corners(fpaths):
-    pass
-
-
 
 def calc_rgb_corr(fpaths):
     
@@ -67,10 +84,6 @@ def calc_rgb_corr(fpaths):
 
     d = dict(category=categories, fpath=fpaths, feat_corr_rg=corr_rg, feat_corr_rb=corr_rb, feat_corr_gb=corr_gb)
     return pd.DataFrame(d)
-
-def calc_image_size(fpaths):
-    for fpath in fpaths:
-
 
 def map_feature_calculation(func, fpaths, func_name):
     # split fpaths for multiprocessing
@@ -94,3 +107,17 @@ def split_seq(seq, size):
         newseq.append(seq[int(round(i*splitsize)):
         int(round((i+1)*splitsize))])
     return newseq
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+for i, fpath in enumerate(fpaths[:3000:100]):
+    imread(fpath)
+    img = rgb2gray(imread(fpath))
+    IMG = np.fft.rfft2(img)
+    ax1.imshow(img)
+    ax2.imshow(np.log(np.abs(IMG)))
+    fig.savefig('tmp_%3.3u.jpg' % i)
+    ax1.cla(); ax2.cla()
+
+
+
