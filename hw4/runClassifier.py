93f8e39f9f5d1306ef20f04d00d344b3aac00b71
Robert Gibboni <robertg@berkeley.edu>
2013-10-01 16:35:35 -0700

    generalized importing of precomputed features

--- hw4/runClassifier.py
+++ hw4/runClassifier.py
@@ -2,13 +2,8 @@ import numpy as np
 from glob import glob
 import os
 import pandas as pd
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score, KFold
-from scipy import ndimage as ndi
-import skimage as ski
-from sklearn.metrics import accuracy_score
-
 import calcFeatures
 
 basedir = '/Users/robert/Documents/Code/pythonwork/AY250/python-seminar/Homeworks/AY250_HW/hw4'
@@ -16,17 +11,22 @@ imgdir = os.path.join(basedir, '50_categories')
 
 if __name__=='__main__':
 
+    '''
+    Load the computed features (corr_rgb, power_hist, and HOG), compile them
+    into a DataFrame, build and test the classifier with cross-validation
+    '''
+
     categories = [os.path.split(i)[1] for i in glob(os.path.join(basedir, '50_categories', '*'))]
     ncategories = len(categories)
 
     fpaths = calcFeatures.get_fpaths(os.path.join(basedir, '50_categories'))
 
     # load previously calculated features
-    corr_rgb = pd.read_csv('corr_rgb.csv', index_col=0)
-    power_hist = pd.read_csv('power_hist.csv', index_col=0)
-    hog = pd.read_csv('hog.csv', index_col=0)
-    df = corr_rgb.join(power_hist)
-    df = df.join(hog)
+    featpaths = glob('feat_*.csv')
+    df = pd.DataFrame()
+    for featpath in featpaths:
+        df = df.join(pd.read_csv(featpath, index_col=0), how='outer')
+
     nimgs = len(df)
     X = df.filter(regex='feat_').values
     Y = df.category.values
