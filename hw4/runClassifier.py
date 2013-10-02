c31ea1b2d40811fede17f9ad50d3ac3f89f10292
Robert Gibboni <robertg@berkeley.edu>
2013-10-02 12:21:20 -0700

    added calculation of top 3 features

--- hw4/runClassifier.py
+++ hw4/runClassifier.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score, KFold
+from sklearn.grid_search import GridSearchCV
 import calcFeatures; reload(calcFeatures)
 import pickle
 
@@ -35,6 +36,25 @@ def run_final_classifier(testimgdir):
     for fpath, y_hat in zip(df.index, Y_hat):
         print '%25s | %-20s' % (fpath, y_hat)
 
+def load_features():
+    
+    df = pd.DataFrame()
+
+    featpaths = glob('feat_*.csv')
+    for i, featpath in enumerate(featpaths):
+        print 'Loading %u of %u features: %s' % (i, len(featpaths), featpath)
+        df = df.join(pd.read_csv(featpath, index_col=0), how='outer')
+    return df
+
+def compute_features():
+
+    df = pd.DataFrame()
+
+    df = df.join(calcFeatures.run_rgb_corr(fpaths, save=False), how='outer')
+    df = df.join(calcFeatures.calc_hog(fpaths, save=False), how='outer')
+    df = df.join(calcFeatures.calc_spatial_power_hist(fpaths, save=False), how='outer')
+
+    return df
 
 def train(load_precomputed=True):
 
@@ -47,23 +67,18 @@ def train(load_precomputed=True):
     ncategories = len(categories)
 
     fpaths = calcFeatures.get_fpaths(os.path.join(basedir, '50_categories'))
-    fpaths = fpaths[::130]
 
-    df = pd.DataFrame()
     if load_precomputed:
         # load previously calculated features
-        featpaths = glob('feat_*.csv')
-        for featpath in featpaths:
-            df = df.join(pd.read_csv(featpath, index_col=0), how='outer')
+        df = load_features()
     else:
-        df = df.join(calcFeatures.run_rgb_corr(fpaths, save=False), how='outer')
-        df = df.join(calcFeatures.calc_hog(fpaths, save=False), how='outer')
-        df = df.join(calcFeatures.calc_spatial_power_hist(fpaths, save=False), how='outer')
+        df = compute_features()
 
     df['category'] = [i.split('_')[0] for i in df.index]
-    
+
     nimgs = len(df)
-    X = df.filter(regex='feat_').values
+    feat_df = df.filter(regex='feat_')
+    X = feat_df.values
     Y = df.category.values
 
     # shuffle dataset
@@ -75,19 +90,36 @@ def train(load_precomputed=True):
     Y = np.array(Y)
 
     # initialize RFC
-    clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
+    # print 'Running grid search on parameters.'
+    # parameters = {'n_estimators':[20,50,100],  'max_features':[8,10]}
+    # clf = GridSearchCV(RandomForestClassifier(), param_grid=parameters, cv=5, refit=True)
+    clf = RandomForestClassifier(n_estimators=50)
 
     # train and evaluate, with cross validation
     scores = cross_val_score(clf, X, Y, cv=KFold(n=nimgs, n_folds=5))
 
+    print 'Fitting the model.'
     # fit the model
     clf.fit(X, Y)
 
-    # save the model to a pickle
-    f = open('model_2.p', 'w')
-    pickle.dumps(clf, f)
-    f.close()
-
     print 'Mean accuracy score: %.4f +/- %.4f %%' % (100*np.mean(scores), 100*np.std(scores))
     print 'Accuracy with random guessing: %.4f %%' % (100./ncategories)
+
+    # find the 3 most important features
+    top3_features_ix = clf.feature_importances_.argsort()[::-1][:3]
+    top3_features_importances = clf.feature_importances_[top3_features_ix]
+    top3_features = feat_df.columns[top3_features_ix]
+
+    print '%11s %-12s %s' % ('', 'name', 'importance')
+    print ' '*8+'-'*40
+    for i, (feat_name, feat_imp) in enumerate(zip(top3_features, top3_features_importances)):
+        print '%10u. %s  %-.7f' % (i+1, feat_name, feat_imp)
+
+
+    print 'Saving the model.'
+    # save the model to a pickle
+    f = open('model.p', 'w')
+    pickle.dump(clf, f)
+    f.close()
+
 
