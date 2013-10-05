3cb0927b4761ab68d203cc3e38a6744f2662fc2e
Robert Gibboni <galileo@gmail.com>
2013-10-05 13:16:05 -0700

    finished image and method selection, will start to add access to remote method docstrings

--- hw5/hw5_1_client.py
+++ hw5/hw5_1_client.py
@@ -1,14 +1,31 @@
 import xmlrpclib
-import PIL
+import Image
 import os
 from glob import glob
 
 host, port = "", 5021
 server = xmlrpclib.ServerProxy('http://%s:%d' % (host, port))
-available_methods = server.system.listMethods()
 
 clientbasepath = os.getcwd()
 
+# get user's method selection
+available_methods = server.system.listMethods()
+nmethods = len(available_methods)
+print 'Available methods from server:'
+for i, method in enumerate(available_methods):
+	print '\t%u. %s' % (i+1, method)
+
+while True:
+	try:
+		meth_ix = int(raw_input('Select method--> '))
+		if meth_ix>nmethods:
+			print 'There is no method #%u. Please enter a number between 1 and %u.' % (meth_ix, nmethods)
+		else: break
+	except ValueError:
+		print 'Please enter a valid number.'
+
+
+# make a list of available images in this directory
 imgextens = ['jpg', 'jpeg', 'png', 'tif', 'tiff', 'bmp']
 imgpaths = []
 for imgexten in imgextens:
@@ -16,28 +33,24 @@ for imgexten in imgextens:
 
 print 'Images in %s' % clientbasepath
 
+# print available image paths
 for i, imgpath in enumerate(imgpaths):
 	print '\t%u. %s' % (i+1, os.path.split(imgpath)[1])
 
-done = False
-while not done:
+# get user's image selection
+while True:
 	try:
 		img_ix = int(raw_input('Select image--> '))
 		if img_ix>len(imgpaths):
 			print 'There is no image #%u. Please enter a number between 1 and %u.' % (img_ix, len(imgpaths))
-		else:
-			break
+		else: break
 	except ValueError:
-		print 'Please enter a valid number'
+		print 'Please enter a valid number.'
 
+# load the selected image
 imgpath = imgpaths[i-1]
 img = Image.open(imgpath)
 
-print 'Available methods from server:'
-for i, method in enumerate(available_methods):
-	print '\t%u. %s' % (i+1, method)
-meth_ix = raw_input('Select method--> ')
 
-result = 
-
+eval('server.%s(img)' % available_methods[meth_ix-1])
 
