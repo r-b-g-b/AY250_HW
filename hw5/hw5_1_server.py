827cb81c4bfefde514835c657437d7797287b70a
Robert Gibboni <galileo@gmail.com>
2013-10-05 11:45:31 -0700

    refined file saving, now there's a saveImages method

--- hw5/hw5_1_server.py
+++ hw5/hw5_1_server.py
@@ -3,21 +3,22 @@ import PIL
 import os
 from glob import glob
 
-basedir = os.getcwd()
-
 class messWithImage:
+
+	def __init__(self):
+
+		self.serverbasedir = os.getcwd()
+
 	def tile(self, img, imgpath=None, nx=4, ny=4):
 
 		print 'Image has been tiled!'
 		img2 = img.copy()
 
-		if imgpath is None:
-			imgpath = get_next_filepath(basedir, 'ServerFlipImage_')
-		img2.save(imgpath)
-	
+		outpath, outpath2 = saveImages(img, img2)
+
 		return img2
 
-	def flip(self, img, imgpath=None, axis=0):
+	def flip(self, img, axis=0):
 		'''
 		Flips the image over its horizontal (0) or vertical (1) axis
 		Input:
@@ -27,23 +28,11 @@ class messWithImage:
 		img2 = img.copy()
 
 		if axis==0:
-			img2 = img2[::-1, :, ...]
+			img2 = img2.transpose(Image.FLIP_LEFT_RIGHT)
 		elif axis==1:
-			img2 = img2[:, ::-1, ...]
+			img2 = img2.transpose(Image.FLIP_TOP_BOTTOM)
 
-		if imgpath is None:
-			imgpath = 'ServerImageFlip_'
-
-
-		imgpath_base, imgpath_exten = os.path.splitext(imgpath)
-		imgpath_serv_base = imgpath_base+'_server_original_'
-		imgpath_serv_base2 = imgpath_base+'_server_flipped_'
-
-		imgpath_serv = get_next_filepath(prefix=imgpath_serv_base, exten=imgpath_exten)
-		imgpath_serv2 = get_next_filepath(prefix=imgpath_serv_base2, exten=imgpath_exten)
-
-		img.save(os.path.join(basedir, imgpath_serv))
-		img2.save(os.path.join(basedir, imgpath_serv2))
+		outpath, outpath2 = saveImages(img, img2)
 
 		return img2
 
@@ -53,13 +42,25 @@ class messWithImage:
 		i = 0
 		while not done:
 			relpath = '%s%3.3u%s' % (prefix, i, exten)
-			abspath = os.path.join(basedir, relpath)
+			abspath = os.path.join(self.serverbasedir, relpath)
 			if os.path.exists(abspath):
 				i+=1
 			else:
 				return relpath
 
+	def saveImages(img, img2):
 
+		imgpath_base, imgpath_exten = os.path.splitext(img.filename)
+		imgpath_serv_base = imgpath_base+'_server_original_'
+		imgpath_serv_base2 = imgpath_base+'_server_flipped_'
+
+		imgpath_serv = get_next_filepath(prefix=imgpath_serv_base, exten=imgpath_exten)
+		imgpath_serv2 = get_next_filepath(prefix=imgpath_serv_base2, exten=imgpath_exten)
+
+		img.save(os.path.join(self.serverbasedir, imgpath_serv))
+		img2.save(os.path.join(self.serverbasedir, imgpath_serv2))
+
+		return imgpath_serv, imgpath_serv2
 
 
 
