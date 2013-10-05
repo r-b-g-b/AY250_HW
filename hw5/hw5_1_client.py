ce03802a044db60aef5df84ddea27e9b38b52933
Robert Gibboni <galileo@gmail.com>
2013-10-05 15:13:35 -0700

    established working version with ugly GUI

--- hw5/hw5_1_client.py
+++ hw5/hw5_1_client.py
@@ -1,21 +1,19 @@
 import wx
 import xmlrpclib
-import Image
+from scipy.ndimage import imread
 import os
 from glob import glob
-
-host, port = "", 5021
-server = xmlrpclib.ServerProxy('http://%s:%d' % (host, port))
-
+import Image
+from numpy import array, uint8
 
 class Frame(wx.Frame):
 	
 	def __init__(self, title, server):
 
-		wx.Frame.__init__(self, None, title=title, size=(350, 300))
+		wx.Frame.__init__(self, None, title=title, size=(540, 150))
 		
 		self.server = server
-		self.methods = server.available_methods()
+		self.methods = server.system.listMethods()
 		# set base directory
 		self.clientbasedir = os.getcwd()
 
@@ -44,16 +42,18 @@ class Frame(wx.Frame):
 		box.Add(m_fdialog, 0, wx.ALL, 10)
 
 		# add a method select button
-		method_list = wx.ListCtrl(panel, label='Choose method')
+		method_list = wx.ListCtrl(panel)
 		method_list.InsertColumn(0, 'Method')
 		for i, method in enumerate(self.methods):
-			method_list.InsertStringItem(i, method)
+			if not method.startswith('_') and not method.startswith('system'):
+				method_list.InsertStringItem(i, method)
 		box.Add(method_list, 0, wx.ALL, 10)
+		method_list.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnSelectMethod)
 
 		method_button = wx.Button(panel, label='Apply method to image')
-		method_button.Bind(EVT_BUTTON, self.OnRun)
+		method_button.Bind(wx.EVT_BUTTON, self.OnRun)
 
-		box.Add(m_method, 0, wx.ALL, 10)
+		box.Add(method_button, 0, wx.ALL, 10)
 
 		# add a close button
 		m_close = wx.Button(panel, wx.ID_CLOSE, 'Close')
@@ -84,9 +84,37 @@ class Frame(wx.Frame):
 			self.imgpath = dlg.GetPath()
 		dlg.Destroy()
 
-	def OnRun(self, event):
-		pass
+		self.img = Image.open(self.imgpath)
+		self.imgshape = list(self.img.size)
+		self.imgshape.extend([len(self.img.getbands())])
+		self.imglist = array(self.img).flatten().tolist()
 
+	def OnSelectMethod(self, event):
+		self.method = self.methods[event.m_itemIndex]
+
+	def OnRun(self, event):
+
+		if not hasattr(self, 'img'):
+			dlg = wx.MessageDialog(self, 'Please select an image.', 'Confirm', wx.OK)
+			result = dlg.ShowModal()
+			dlg.Destroy()
+			return
+		if not hasattr(self, 'method'):
+			dlg = wx.MessageDialog(self, 'Please select a method.', 'Confirm', wx.OK)
+			result = dlg.ShowModal()
+			dlg.Destroy()
+			return
+
+		imglist2, imgshape2, suffix = eval('server.%s(self.imglist, self.imgshape, self.imgpath)' % self.method)
+		self.img2 = Image.fromarray(array(imglist2).astype(uint8).reshape(imgshape2))
+		
+		# save images
+		img_base, img_exten = os.path.splitext(self.imgpath)
+		self.img.save(os.path.join('%s_client_original_%3.3u%s' % (img_base, suffix, img_exten)))
+		self.img2.save(os.path.join('%s_client_%s_%3.3u%s' % (img_base, self.method, suffix, img_exten)))
+
+host, port = "", 5021
+server = xmlrpclib.ServerProxy('http://%s:%d' % (host, port), allow_none=True)
 
 app = wx.App(redirect=True)
 top = Frame('XML-RCP Server Client', server)
--- hw5/hw5_1_server.py
+++ hw5/hw5_1_server.py
@@ -1,7 +1,9 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-import PIL
+from numpy import array, uint8
+import Image
 import os
 from glob import glob
+
 
 class messWithImage:
 
@@ -9,22 +11,27 @@ class messWithImage:
 
 		self.serverbasedir = os.getcwd()
 
-	def tile(self, img, imgpath=None, nx=4, ny=4):
+	def tile(self, imglist, imgshape, imgpath, nx=4, ny=4):
 
+		img = Image.fromarray(array(imglist).astype(uint8).reshape(imgshape))
 		print 'Image has been tiled!'
 		img2 = img.copy()
 
-		outpath, outpath2 = saveImages(img, img2)
+		outpath, outpath2, suffix = self._saveImages(img, img2, imgpath, 'tile')
+		
+		imgshape2 = list(img2.size)
+		imgshape2.extend([len(img2.getbands())])
+		
+		return array(img2).flatten().tolist(), imgshape2, suffix
 
-		return img2
-
-	def flip(self, img, axis=0):
+	def flip(self, imglist, imgshape, imgpath, axis=0):
 		'''
 		Flips the image over its horizontal (0) or vertical (1) axis
 		Input:
 			img : the image array
 			imgpath : relative filepath for the original image
 		'''
+		img = Image.fromarray(array(imglist).astype(uint8).reshape(imgshape))
 		img2 = img.copy()
 
 		if axis==0:
@@ -32,44 +39,46 @@ class messWithImage:
 		elif axis==1:
 			img2 = img2.transpose(Image.FLIP_TOP_BOTTOM)
 
-		outpath, outpath2 = saveImages(img, img2)
+		outpath, outpath2, suffix = self._saveImages(img, img2, imgpath, 'flip')
 
-		return img2
+		imgshape2 = list(img2.size)
+		imgshape2.extend([len(img2.getbands())])
+		return array(img2).flatten().tolist(), imgshape2, suffix
 
-	def get_next_filepath(prefix='ServerImage_', exten='.png'):
+	def _get_next_filepath(self, prefix='ServerImage_', exten='.png'):
 		'''
 		Given a file prefix, this function returns an unused filepath
 		in the base directory
 		'''
 
 		done = False
-		i = 0
+		suffix = 0
 		while not done:
-			relpath = '%s%3.3u%s' % (prefix, i, exten)
+			relpath = '%s%3.3u%s' % (prefix, suffix, exten)
 			abspath = os.path.join(self.serverbasedir, relpath)
 			if os.path.exists(abspath):
-				i+=1
+				suffix+=1
 			else:
-				return relpath
+				return relpath, suffix
 
-	def saveImages(img, img2):
+	def _saveImages(self, img, img2, imgpath, modtype):
 		'''
 		Given the original and modified images (type PIL Images)
 		this function will built appropriate filepaths and save the
 		images
 		'''
 
-		imgpath_base, imgpath_exten = os.path.splitext(img.filename)
+		imgpath_base, imgpath_exten = os.path.splitext(imgpath)
 		imgpath_serv_base = imgpath_base+'_server_original_'
-		imgpath_serv_base2 = imgpath_base+'_server_flipped_'
+		imgpath_serv_base2 = imgpath_base+'_server_%s_' % modtype
 
-		imgpath_serv = get_next_filepath(prefix=imgpath_serv_base, exten=imgpath_exten)
-		imgpath_serv2 = get_next_filepath(prefix=imgpath_serv_base2, exten=imgpath_exten)
+		imgpath_serv, suffix = self._get_next_filepath(prefix=imgpath_serv_base, exten=imgpath_exten)
+		imgpath_serv2, suffix = self._get_next_filepath(prefix=imgpath_serv_base2, exten=imgpath_exten)
 
 		img.save(os.path.join(self.serverbasedir, imgpath_serv))
 		img2.save(os.path.join(self.serverbasedir, imgpath_serv2))
 
-		return imgpath_serv, imgpath_serv2
+		return imgpath_serv, imgpath_serv2, suffix
 
 
 
@@ -82,4 +91,7 @@ server.register_introspection_functions()
 
 print "Server starting at: ", host, port
 
-server.serve_forever()
+try:
+	server.serve_forever()
+except:
+	server.close()
