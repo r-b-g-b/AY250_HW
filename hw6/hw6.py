c885fa4bcc5b5ed0fcb450852ca25dde004434ca
Robert Gibboni <galileo@gmail.com>
2013-10-16 16:04:13 -0700

    added plotting functions

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -5,11 +5,140 @@ import urllib2
 import numpy as np
 
 
-def run():
+def calc_latlon_offset():
+
+
+	icaos = get_airportcodes(connection)
+	nairports = len(icaos)
+
+	df = pd.DataFrame(columns=('code1', 'code2', 'dist', 'dlat', 'dlon'))
+
+	for i in range(nairports):
+		for j in range(i, nairports):
+			icao1 = icaos[i]
+			icao2 = icaos[j]
+			lat1, lon1 = get_latlon(connection, icao1)
+			lat2, lon2 = get_latlon(connection, icao2)
+			if i==j:
+				dist=0
+			else:
+				dist = np.abs(distance_on_unit_sphere(lat1, lon1, lat2, lon2)*6373)
+			dlat = lat2-lat1
+			dlon = lon2-lon1
+			df = df.append(dict(code1=icao1, code2=icao2, dist=dist, dlat=dlat, dlon=dlon), ignore_index=True)
+			if i!=j:
+				df = df.append(dict(code1=icao2, code2=icao1, dist=dist, dlat=-dlat, dlon=-dlon), ignore_index=True)
+
+	df['dlat_bin'] = pd.cut(df.dlat, bins=np.arange(-30, 35, 5))
+	df['dlon_bin'] = pd.cut(df.dlon, bins=np.arange(-100, 110, 10))
+
+	return df
+
+
+def plot_by_distance(df):
 	'''
-	Calculates correlation coefficients for each pair of top airports
+	Plots the correlations in the changes of temperature and cloud coverage
+	as a function of distance. The first point (distance=0) shows how a city's
+	weather predicts its own future weather.
 	'''
-	connection = sqlite3.connect('airports.db')
+
+	distbins = np.hstack((-1, np.arange(1, 10000, 500)))
+	ndists = len(distbins)-1
+	y_temp = np.zeros((ndists, 3))
+	y_cloud = np.zeros_like(y_temp)
+
+	lags = [1, 3, 7]
+	for i in range(ndists):
+		ix = np.vstack((distbins[i]<df.dist, df.dist<distbins[i+1])).all(0)
+		if ix.sum()>0:
+			for k, lag in enumerate(lags):
+				y_temp[i, k] = np.mean(df.ix[ix]['temp_corr%i' % lag])
+				y_cloud[i, k] = np.mean(df.ix[ix]['cloud_corr%i' % lag])
+
+	fig, axs = plt.subplots(2, 1)
+	axs[0].plot(distbins[:-1], y_temp, marker='.')
+	axs[0].set_title('Max temperature')
+	axs[1].plot(distbins[:-1], y_cloud, marker='.')
+	axs[1].set_title('Cloud cover')
+	[a.legend(['%i days' % lag for lag in lags]) for a in axs]
+	axs[1].set_xlabel('Distance (km)')
+	axs[1].set_ylabel('Correlation coefficient')
+
+
+def plot_correlation_matrix(df):
+
+	latbins = np.arange(-30, 35, 5)
+	lonbins = np.arange(-100, 110, 10)
+	nlats = len(latbins)-1
+	nlons = len(lonbins)-1
+	
+	lags = [1, 3, 7]
+	nlags = len(lags)
+	cmat_temp = np.zeros((nlats, nlons, 3))
+	cmat_cloud = np.zeros_like(cmat_temp)
+	for i in range(nlats):
+		for j in range(nlons):
+			ix = np.vstack((latbins[i]<df.dlat, df.dlat<latbins[i+1],
+				lonbins[j]<df.dlon, df.dlon<lonbins[j+1])).all(0)
+			if ix.sum()>0:
+				for k, lag in enumerate(lags):
+					cmat_temp[i, j, k] = np.mean(df.ix[ix]['temp_corr%i' % lag])
+					cmat_cloud[i, j, k] = np.mean(df.ix[ix]['cloud_corr%i' % lag])
+
+	fig, axs = plt.subplots(nlags, 2)
+	fig.suptitle('Max temperature')
+	for i, lag in enumerate(lags):
+		axs[i, 0].imshow(cmat_temp[..., i],
+			interpolation='nearest',
+			extent=(lonbins[0], lonbins[-1], latbins[0], latbins[-1]),
+			vmin=-0.2, vmax=0.2,
+			aspect='auto')
+		axs[i, 0].set_title('Max temp %i day lag' % lag)
+		img = axs[i, 1].imshow(cmat_cloud[..., i],
+			interpolation='nearest',
+			extent=(lonbins[0], lonbins[-1], latbins[0], latbins[-1]),
+			vmin=-0.2, vmax=0.2,
+			aspect='auto')
+		axs[i, 1].set_title('Cloud cover %i day lag' % lag)
+	fig.tight_layout()
+	plt.colorbar(img, ax=axs[0,0])
+
+def combine_latlon_weatherdata():
+
+	df = calc_latlon_offset()
+
+	tmp = np.load('C.npz')
+	C_temp = tmp['C_temp']
+	C_cloud = tmp['C_cloud']
+	icaos = tmp['ICAOs']
+
+	temp_corr1 = []
+	temp_corr3 = []
+	temp_corr7 = []
+	cloud_corr1 = []
+	cloud_corr3 = []
+	cloud_corr7 = []
+	for ix, row in df.iterrows():
+		i = (icaos==row['code1']).nonzero()[0][0]
+		j = (icaos==row['code2']).nonzero()[0][0]
+		temp_corr1.append(C_temp[i, j, 0])
+		temp_corr3.append(C_temp[i, j, 1])
+		temp_corr7.append(C_temp[i, j, 2])
+		cloud_corr1.append(C_cloud[i, j, 0])
+		cloud_corr3.append(C_cloud[i, j, 1])
+		cloud_corr7.append(C_cloud[i, j, 2])
+
+	df['temp_corr1'] = temp_corr1
+	df['temp_corr3'] = temp_corr3
+	df['temp_corr7'] = temp_corr7
+	df['cloud_corr1'] = cloud_corr1
+	df['cloud_corr3'] = cloud_corr3
+	df['cloud_corr7'] = cloud_corr7
+
+	return df
+
+def get_airportcodes(connection):
+	
 	cursor = connection.cursor()
 
 	cmd = """
@@ -18,9 +147,22 @@ def run():
 	"""
 	cursor.execute(cmd)
 	icaos, = zip(*cursor.fetchall())
+	return icaos
+
+def calculate_correlations():
+	'''
+	Calculates correlation coefficients in changes of 
+	max temperature and cloud cover for each pair of
+	top airports.
+	'''
+
+	connection = sqlite3.connect('airports.db')
+	cursor = connection.cursor()
+
+	icaos = get_airportcodes(connection)
 
 	nairports = len(icaos)
-	lags = [3]
+	lags = [1, 3, 5]
 	nlags = len(lags)
 	C_temp = np.empty((nairports, nairports, nlags))
 	C_cloud = np.empty((nairports, nairports, nlags))
@@ -34,9 +176,7 @@ def run():
 
 			# get weather data, lat/lon for the two locations
 			datestr1,temp1,cloud1 = get_weatherdata(connection, icao1)
-			lat1, lon1 = get_latlon(connection, icao1)
 			datestr2,temp2,cloud2 = get_weatherdata(connection, icao2)
-			lat2, lon2 = get_latlon(connection, icao2)
 
 			# remove missing entries
 			dtemp1 = np.diff(clean_entry(temp1))
@@ -288,6 +428,43 @@ def add_weatherdata(connection):
 	connection.commit()
 
 	return connection
+
+
+import math
+
+def distance_on_unit_sphere(lat1, long1, lat2, long2):
+	'''
+	http://www.johndcook.com/python_longitude_latitude.html
+	'''
+	# Convert latitude and longitude to 
+	# spherical coordinates in radians.
+	degrees_to_radians = math.pi/180.0
+
+	# phi = 90 - latitude
+	phi1 = (90.0 - lat1)*degrees_to_radians
+	phi2 = (90.0 - lat2)*degrees_to_radians
+
+	# theta = longitude
+	theta1 = long1*degrees_to_radians
+	theta2 = long2*degrees_to_radians
+
+	# Compute spherical distance from spherical coordinates.
+
+	# For two locations in spherical coordinates 
+	# (1, theta, phi) and (1, theta, phi)
+	# cosine( arc length ) = 
+	#    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+	# distance = rho * arc length
+
+	cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+	       math.cos(phi1)*math.cos(phi2))
+	arc = math.acos( cos )
+
+	# Remember to multiply arc by the radius of the earth 
+	# in your favorite set of units to get length.
+	return arc
+
+
 
 def build_database():
 	
