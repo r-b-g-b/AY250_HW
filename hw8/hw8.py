d1a227349eb210a63f10e278619e795e8fa50e63
Robert Gibboni <galileo@gmail.com>
2013-10-29 13:22:57 -0700

    added image retrieval from web, saving image to disk because i can't find out how to read the binary directly

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -25,15 +25,25 @@ class ImageManip(HasTraits):
 	manips = ['Refresh', 'Blur', 'Sharpen']
 
 	def _submit_fired(self):
+		'''
+		Sets self.imgurl to the first result of a Google image search for the string
+		stored in self.query
+		'''
 
 		queryurl = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s'
-		request = urllib2.Request(queryurl%query, None, {'Referer': 'http://www.github.com/gibbonorbiter'})
+		request = urllib2.Request(queryurl%self.query, None, {'Referer': 'http://www.github.com/gibbonorbiter'})
 		response = urllib2.urlopen(request)
 
 		results = json.load(response)
-		imgurl = results['responseData']['results'][0]['url']
+		self.imgurl = results['responseData']['results'][0]['url']
 
-	view = View('query', Item('submit', show_label=False), 'imgurl')
+		imgresult = results['responseData']['results'][0]
+		height,  width = int(imgresult['height']), int(imgresult['width'])
+		# imgraw = requests.get(imgurl).content
+		img = Image.fromstring('RGB', (height, width), imgraw)
+
+
+	view = View('query', Item('submit', show_label=False), 'imgurl', buttons=['Cancel', 'OK'])
 
 
 gui = ImageManip(query='')
