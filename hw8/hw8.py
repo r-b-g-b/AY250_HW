29fd33a4d39ea01e101089abe5e73bbdfebfb086
Robert Gibboni <galileo@gmail.com>
2013-10-29 13:04:50 -0700

    trying out ways to get url search to work

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -1,23 +1,40 @@
 try:
     from enthought.traits.api import HasTraits, Str, \
-            Int, Float, Enum, DelegatesTo, This,Instance
+            Int, Float, Enum, DelegatesTo, This, Instance, \
+            Button
     from enthought.traits.ui.api import View, Item, Group
 except:
     from traits.api import HasTraits, Str, \
-            Int, Float, Enum, DelegatesTo,This,Instance
+            Int, Float, Enum, DelegatesTo, This, Instance, \
+            Button
     from traitsui.api import View, Item, Group
 
 
-class SearchQuery(HasTraits):
+import urllib2
+import json
 
-	pass
+class ImageManip(HasTraits):
+	
+	query = Str('')
+	submit = Button('Submit')
 
-class ImgURL(HasTraits):
+	imgurl = Str('')
+	
+	imgdisplay = None
 
-	pass
+	manips = ['Refresh', 'Blur', 'Sharpen']
 
-class ImgDisplay(HasTraits):
-	pass
+	def _submit_fired(self):
 
-class ImgManip(HasTraits):
-	pass
+		queryurl = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s'
+		request = urllib2.Request(queryurl%query, None, {'Referer': 'http://www.github.com/gibbonorbiter'})
+		response = urllib2.urlopen(request)
+
+		results = json.load(response)
+		imgurl = results['responseData']['results'][0]['url']
+
+	view = View('query', Item('submit', show_label=False), 'imgurl')
+
+
+gui = ImageManip(query='')
+gui.configure_traits()
